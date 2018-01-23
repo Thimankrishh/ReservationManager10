e29c4de9aa4cfac54721c61b3de2e74f890b31c4
Thimankrishh <mysoftware15@gmail.com>
2018-01-23 19:18:06 +0530

    Initial commit

---
+++ app/src/main/java/com/thiman/android/reservationmanager10/MainActivity.java
@@ -0,0 +1,66 @@
+package com.thiman.android.reservationmanager10;
+
+import android.app.ProgressDialog;
+import android.content.Intent;
+import android.net.Uri;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.LinearLayout;
+import android.widget.TextView;
+
+import com.rengwuxian.materialedittext.MaterialEditText;
+
+public class MainActivity extends AppCompatActivity {
+
+    LinearLayout lolgn;
+    Animation fadeIn;
+    Button btnLogin;
+    EditText edtPhnNo, edtPwd;
+    TextView signIn;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+
+        lolgn = findViewById(R.id.lolgn);
+
+
+        fadeIn = AnimationUtils.loadAnimation(this,R.anim.imageanim);
+        lolgn.setAnimation(fadeIn);
+
+        btnLogin = findViewById(R.id.btn_login);
+        signIn = findViewById(R.id.link_to_sign_up);
+
+        edtPhnNo =(MaterialEditText)findViewById(R.id.edt_phn);
+        edtPwd = (MaterialEditText)findViewById(R.id.edt_pwd);
+
+        btnLogin.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                final ProgressDialog mDialog = new ProgressDialog(MainActivity.this);
+                mDialog.setMessage("Please Waiting");
+                mDialog.show();
+
+//                Intent homeIntent = new Intent(MainActivity.this,Home.class);
+//                //    Common.currentUser= user;
+//                homeIntent.setFlags(Intent.FLAG_ACTIVITY_CLEAR_TASK| Intent.FLAG_ACTIVITY_NEW_TASK);
+//                startActivity(homeIntent);
+//                finish();
+            }
+        });
+        signIn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent signIn = new Intent(Intent.ACTION_VIEW, Uri.parse("https://www.google.lk/"));
+                startActivity(signIn);
+            }
+        });
+    }
+}
---
+++ app/src/main/java/com/thiman/android/reservationmanager10/SplashActivity.java
@@ -0,0 +1,67 @@
+package com.thiman.android.reservationmanager10;
+import android.content.Intent;
+import android.media.Image;
+import android.os.Bundle;
+import android.support.graphics.drawable.AnimationUtilsCompat;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
+import android.widget.ImageView;
+import android.widget.RelativeLayout;
+import android.widget.TextView;
+
+import java.util.Timer;
+import java.util.TimerTask;
+
+/**
+ * Created by Asus on 12/24/2017.
+ */
+
+public class SplashActivity extends AppCompatActivity {
+
+    TextView reservation;
+    TextView manager;
+    ImageView res;
+    RelativeLayout r1;
+    RelativeLayout r2;
+    Animation fadeIn;
+    Animation fromBottom;
+    Animation fromTop;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        setContentView(R.layout.splash_activity);
+
+        manager = findViewById(R.id.second);
+        fromBottom = AnimationUtils.loadAnimation(this,R.anim.imageanim);
+        manager.setAnimation(fromBottom);
+
+        reservation = findViewById(R.id.first);
+        fromTop = AnimationUtils.loadAnimation(this,R.anim.imageanim);
+        reservation.setAnimation(fromTop);
+
+        res = findViewById(R.id.res);
+        fadeIn = AnimationUtils.loadAnimation(this,R.anim.from_top);
+        res.setAnimation(fadeIn);
+
+        r1 = findViewById(R.id.r1);
+        fadeIn = AnimationUtils.loadAnimation(this,R.anim.imageanim);
+        r1.setAnimation(fadeIn);
+
+        r2 = findViewById(R.id.r2);
+        fadeIn = AnimationUtils.loadAnimation(this,R.anim.imageanim);
+        r2.setAnimation(fadeIn);
+
+        new Timer().schedule(new TimerTask() {
+            @Override
+            public void run() {
+                startActivity(new Intent(getApplicationContext(), MainActivity.class));
+            }
+        } ,4000);
+    }
+
+
+}
